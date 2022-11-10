f193e8df57cba9ab5bf70babc4b6a46c18021ecd
Your Name <jlmeeks587@gmail.com>
2022-11-10 16:15:53 -0500

    Refactored code to use React lifecycle methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
-
-const state = {
-  robots: robots,
-  searchfield: '',
-}
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }));
   }
   
   onSearchChange = (event) => {
